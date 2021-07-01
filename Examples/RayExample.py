f42d26ee68c494a346001e62237dafa1d35ced40
Tobias Machnitzki <tobias.machnitzki@hzg.de>
2021-07-01 11:51:55 +0200

    Add Examples

---
+++ Examples/RayExample.py
@@ -0,0 +1,26 @@
+import ray
+from ray.util.joblib 
+import register_ray  # noqa: E402
+
+
+# lets define a toy problem:
+def costly_simulation(list_param):
+    time.sleep(np.random.random())
+    return sum(list_param)
+
+input_params = pd.DataFrame(np.random.random(size=(500, 4)),
+                            columns=['param_a', 'param_b', 'param_c', 'param_d'])
+
+
+# now we use the ray cluster to run the problem:
+ray.init(address=os.getenv("RAY_ADDRESS"), _redis_password=os.getenv("REDIS_PWD"))
+register_ray()
+print("Starting ray loop:")
+with parallel_backend("ray"):
+    parallel_results = Parallel(verbose=7, n_jobs=-1)(
+        delayed(costly_simulation)(input_param[1].values) for input_param in tqdm(input_params))
+    
+with open("ray_exampel_output", "w") as f:
+    for result in parallel_results:
+        f.write(result)
+        f.write("\n")
---
+++ Examples/ray-setup/SLURM-LAUNCH.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --job-name=preprocess-plankton
+
+### Modify this according to your Ray workload.
+#SBATCH --nodes=2
+
+#SBATCH --ntasks-per-node=1  # do not change
+
+### Modify this according to your Ray workload.
+#SBATCH --cpus-per-task=45
+#SBATCH --time=8:00:00
+#SBATCH --account=machnitz
+#SBATCH --partition=pAll
+#SBATCH --output=slurm_output/slurm-%j.out
+
+
+################# DON NOT CHANGE THINGS HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###############
+redis_password=$(uuidgen)
+export redis_password
+
+nodes=$(scontrol show hostnames $SLURM_JOB_NODELIST) # Getting the node names
+nodes_array=( $nodes )
+
+node_1=${nodes_array[0]}
+ip=$(srun --nodes=1 --ntasks=1 -w $node_1 hostname --ip-address) # making redis-address
+port=6379
+ip_head=$ip:$port
+export ip_head
+echo "IP Head: $ip_head"
+
+export RAY_ADDRESS=$ip_head
+export  REDIS_PWD=$redis_password
+
+echo "STARTING HEAD at $node_1"
+srun --nodes=1 --ntasks=1 -w $node_1 start-head.sh $ip $redis_password &
+sleep 30
+
+worker_num=$(($SLURM_JOB_NUM_NODES - 1)) #number of nodes other than the head node
+for ((  i=1; i<=$worker_num; i++ ))
+do
+  node_i=${nodes_array[$i]}
+  echo "STARTING WORKER $i at $node_i"
+  srun --nodes=1 --ntasks=1 -w $node_i start-worker.sh $ip_head $redis_password &
+  sleep 5
+done
+##############################################################################################
+
+#### call your code below. If you are using anaconda environments I always find it best to
+#### directly call them with full path:
+/gpfs/home/machnitz/miniconda3/envs/plankton/bin/python ../RayExample.py
+exit
